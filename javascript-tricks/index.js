b628d8d15f0e5415a332c0611619f625abdd2245
Sean <sean@vestaridefair.com>
2021-01-10 19:19:40 -0800

    01/10 7:19PM

--- javascript-tricks/index.js
+++ javascript-tricks/index.js
@@ -1,4 +1,25 @@
+//https://github.com/seanmodd/jsUdemy
+
 var arr = [1, 2, 3];
 
 console.log(arr.length);
 //
+
+var obj = { a: 1, b: 2 };
+
+var thisarray = Object.keys(obj);
+console.log(thisarray);
+
+//Iterate Over an Object
+
+var username = {
+  first: 'John',
+  last: 'Doe',
+};
+
+for (let u in username) {
+  if (username.hasOwnProperty(u)) {
+      console.log(u, username[u])
+  }
+    console.log(u);
+}
