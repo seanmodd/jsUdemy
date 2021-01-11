3c770c3e838bdf434938cafe2e6133ae3667d9e5
Sean <sean@vestaridefair.com>
2021-01-10 19:59:41 -0800

    01/10 7:59PM

---
+++ complete-javascript-developer-2020/index.js
@@ -0,0 +1,116 @@
+//https://github.com/seanmodd/jsUdemy
+
+var arr = [1, 2, 3];
+
+console.log(arr.length);
+//
+
+var obj = { a: 1, b: 2 };
+
+var thisarray = Object.keys(obj);
+console.log(thisarray);
+/* 🗓 Section 2 Working with Objects */
+
+/* 3.)📌   Iterate Over an Object */
+var username = {
+  first: 'John',
+  last: 'Doe',
+};
+
+for (let u in username) {
+  if (username.hasOwnProperty(u)) {
+      console.log(u, username[u])
+  }
+    console.log(u);
+}
+
+
+/* 4.)📌 Convert Object to Array */
+
+/* 5.)📌 Check if Property is in Object */
+
+/* 6.)📌 Prevent Object Properties from Being Added / Deleted */
+
+/* 7.)📌 Convert Object to Array */
+
+/* 8.)📌 Convert Object to Array */
+
+/* 9.)📌 Convert Object to Array */
+
+/* 10.)📌 Convert Object to Array */
+
+/* 11.)📌 Convert Object to Array */
+
+/* 12.)📌 Convert Object to Array */
+
+/* 13.)📌 Convert Object to Array */
+
+/* 14.)📌 Convert Object to Array */
+
+/* 15.)📌 Check if Property is in Object */
+
+/* 16.)📌 Prevent Object Properties from Being Added / Deleted */
+
+/* 17.)📌 Convert Object to Array */
+
+/* 18.)📌 Convert Object to Array */
+
+/* 19.)📌 Convert Object to Array */
+
+/* 20.)📌 Convert Object to Array */
+
+/* 21.)📌 Convert Object to Array */
+
+/* 22.)📌 Convert Object to Array */
+
+/* 23.)📌 Convert Object to Array */
+
+/* 24.)📌 Convert Object to Array */
+
+/* 25.)📌 Check if Property is in Object */
+
+/* 26.)📌 Prevent Object Properties from Being Added / Deleted */
+
+/* 27.)📌 Convert Object to Array */
+
+/* 28.)📌 Convert Object to Array */
+
+/* 29.)📌 Convert Object to Array */
+
+/* 30.)📌 Convert Object to Array */
+/* 31.)📌 Convert Object to Array */
+
+/* 32.)📌 Convert Object to Array */
+
+/* 33.)📌 Convert Object to Array */
+
+/* 34.)📌 Convert Object to Array */
+
+/* 35.)📌 Check if Property is in Object */
+
+/* 36.)📌 Prevent Object Properties from Being Added / Deleted */
+
+/* 37.)📌 Convert Object to Array */
+
+/* 38.)📌 Convert Object to Array */
+
+/* 39.)📌 Convert Object to Array */
+
+/* 40.)📌 Convert Object to Array */
+/* 41.)📌 Convert Object to Array */
+
+/* 42.)📌 Convert Object to Array */
+
+/* 43.)📌 Convert Object to Array */
+
+/* 44.)📌 Convert Object to Array */
+
+/* 45.)📌 Check if Property is in Object */
+
+/* 46.)📌 Prevent Object Properties from Being Added / Deleted */
+
+/* 47.)📌 Convert Object to Array */
+
+/* 48.)📌 Convert Object to Array */
+
+
--- javascript-tricks/index.js
+++ javascript-tricks/index.js
@@ -10,8 +10,11 @@ var obj = { a: 1, b: 2 };
 var thisarray = Object.keys(obj);
 console.log(thisarray);
 
-//Iterate Over an Object
+/* 
+🗓 SECTION 2 --- WORKING WITH OBJECTS
+*/
 
+/* 3.)📌   Iterate Over an Object */
 var username = {
   first: 'John',
   last: 'Doe',
@@ -23,3 +26,111 @@ for (let u in username) {
   }
     console.log(u);
 }
+
+
+/* 4.)📌 Convert Object to Array */
+
+/* 5.)📌 Check if Property is in Object */
+
+/* 6.)📌 Prevent Object Properties Being Added */
+
+/* 7.)📌 Prevent Object Properties from Being Added or Deleted */
+
+/* 8.)📌 Prevent Object Properties from Being Changed At All */
+
+/* 9.)📌 Merge Multiple Objects into One Object */
+
+/* 10.)📌 Computed Object Properties */
+
+/* 11.)📌 Delete or Filter Property of Object */
+
+/* 12.)📌 Get all Values in Object */
+
+/* 
+🗓 SECTION 3 --- WORKING WITH ARRAYS
+*/
+
+/* 13.)📌 Shallow Copy and Clone Array */
+
+/* 14.)📌 Get Random Element from Array */
+
+/* 15.)📌 Remove Falsy Values in Array */
+
+/* 16.)📌 Remove Items from Array */
+
+/* 17.)📌 Fill Array with Values */
+
+/* 18.)📌 Find Certain Element and Index or Indices of Array */
+
+/* 19.)📌 Make Range of Numbers within Array */
+
+/* 20.)📌 Get Unique Values in an Array */
+
+/* 21.)📌 Find Difference Between Two Arrays */
+
+/* 22.)📌 Remove Items from Left or Right Side of Array */
+
+/* 23.)📌  Find Shared Values Between Two Arrays */
+
+/* 24.)📌 Get Last Item and or Everything Before in Array */
+
+/* 25.)📌 Return Last Item of Array, Create findLast or findLastIndex Functions */
+
+/* 
+🗓 SECTION 4 --- WORKING WITH FUNCTIONS
+*/
+
+/* 26.)📌 Debounce Function */
+
+/* 27.)📌 'Once' Function */
+
+/* 28.)📌 Measure Time For Function to Run */
+
+/* 
+🗓 SECTION 5 --- WORKING WITH STRINGS
+*/
+
+/* 29.)📌 Check if String Contains Substring */
+
+/* 30.)📌 Iterate Over a String */
+
+/* 31.)📌 Capitalize First Word of String */
+
+/* 32.)📌 Capitalize Every Word in String */
+
+/* 
+🗓 SECTION 6 --- WORKING WITH NUMBERS OR MATH OPERATIONS
+*/
+
+/* 33.)📌 Change Length of Decimals */
+
+/* 34.)📌 Fixing Math Errors with Decimals */
+
+/* 35.)📌 Get Random Integer in Range */
+
+/* 36.)📌 Round Decimals to Integers */
+
+/* 37.)📌 Get Random Decimal in Range */
+
+/* 
+🗓 SECTION 7 --- GENERAL TRICKS
+*/
+
+/* 38.)📌 Shortcircuiting with Logical 'And' */
+
+/* 39.)📌 Use Function Declarations for Greater Flexibility */
+
+/* 40.)📌 Use Ternary Operator */
+
+/* 41.)📌 Use 'break' and 'continue' with For Loops */
+
+/* 
+🗓 SECTION 8 --- ES6 TRICKS
+*/
+
+/* 42.)📌 Private Data Using IIFEs or Block Scope */
+
+/* 43.)📌 Array Destructuring to Swap Variables */
+
+
+
